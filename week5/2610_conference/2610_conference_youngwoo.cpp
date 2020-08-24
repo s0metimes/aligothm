dcd163494eceb5c5b9d06f499fd13324fa9c88ad
geniusnam <youngwoo9706@gmail.com>
2020-08-24 23:21:23 +0900

    conference

--- week5/2610_conference/2610_conference_youngwoo.cpp
+++ week5/2610_conference/2610_conference_youngwoo.cpp
@@ -17,21 +17,6 @@ int gNum = 0;
 int N;
 bool flag = false;
 
-void dfs(int s) {
-    if(g[s] != 0) return;
-    if(!flag) gNum++;
-    g[s] = gNum;
-
-    // printf("s : %d, gNum : %d\n", s, gNum);
-    for(int i = s+1; i <= N; i++) {
-        if(g[i] != 0) continue;
-        if(cost[s][i] == 1) {
-            flag = true;
-            dfs(i);
-        }
-    }
-}
-
 void Grouping() {
     for(int k = 1; k <= gNum; k++) 
         for(int i = 1; i <= N; i++) 
@@ -46,6 +31,14 @@ void Floyd_Whashall() {
             }
         }
     }
+}
+
+void dfs(int s) {
+    if(g[s] != 0) return;
+    g[s] = gNum;
+
+    for(int i = 1; i <= N; i++) 
+        if(cost[s][i] == 1) dfs(i); 
 }
 
 int main(int argc, char const *argv[])
@@ -68,8 +61,9 @@ int main(int argc, char const *argv[])
     }
 
     for(int s = 1; s <= N; s++) {
+        if(g[s] != 0) continue;
+        gNum++; 
         dfs(s);
-        flag = false;
     }
     
     Grouping();
@@ -81,6 +75,7 @@ int main(int argc, char const *argv[])
             if(cost[f][t] == NO) cost[f][t] = 0;
     
     printf("%d\n", gNum);
+    vector< int > result;
     for(int i = 1; i <= gNum; i++) {
         int minV = NO;
         int minIdx = 0;
@@ -91,8 +86,16 @@ int main(int argc, char const *argv[])
                 minIdx = p;
             }
         }
-        printf("%d\n", minIdx);
+        result.push_back(minIdx);
     }
+
+    sort(result.begin(), result.end());
+    // for(auto ceo : result) 
+    //     printf("%d\n", ceo);
+    for(int i = 0; i < result.size(); i++) {
+        printf("%d\n", result[i]);
+    }
+    
     return 0;
 }
 
@@ -171,41 +174,41 @@ int main(int argc, char const *argv[])
 //         flag = false;
 //     }
 
-//     // for(int s = 1; s <= N; s++) 
-//     //     printf("%d ", g[s]);
-//     // printf("\n\n");
+//     for(int s = 1; s <= N; s++) 
+//         printf("%d ", g[s]);
+//     printf("\n\n");
 
 //     Grouping();
 
 //     Floyd_Whashall();
 
-//     // for(int i = 1; i <= gNum; i++) {
-//     //     printf("group num : %d | ", i);
-//     //     for(auto p : group[i]) {
-//     //        printf("%d", p); 
-//     //     }
-//     //     printf("\n");
-//     // }
+//     for(int i = 1; i <= gNum; i++) {
+//         printf("group num : %d | ", i);
+//         for(auto p : group[i]) {
+//            printf("%d", p); 
+//         }
+//         printf("\n");
+//     }
 
 //     for(int f = 1; f <= N; f++) 
 //         for(int t = 1; t <= N; t++) 
 //             if(cost[f][t] == NO) cost[f][t] = 0;
 
-//     // for(int f = 1; f <= N; f++) {
-//     //     for(int t = 1; t <= N; t++) {
-//     //         printf("%d", cost[f][t]);
-//     //         if(t != N) printf(" ");
-//     //     }
-//     //     printf("\n");
-//     // }
-//     // printf("\n");
+//     for(int f = 1; f <= N; f++) {
+//         for(int t = 1; t <= N; t++) {
+//             printf("%d", cost[f][t]);
+//             if(t != N) printf(" ");
+//         }
+//         printf("\n");
+//     }
+//     printf("\n");
 //     printf("%d\n", gNum);
 //     for(int i = 1; i <= gNum; i++) {
 //         int minV = NO;
 //         int minIdx = 0;
 //         for(auto p : group[i]) {
-//             // printf("i = %d, p = %d\n", i, p);
-//             // printf("minV : %d max_element : %d\n", minV, *max_element(cost[p].begin(), cost[p].end()));
+//             printf("i = %d, p = %d\n", i, p);
+//             printf("minV : %d max_element : %d\n", minV, *max_element(cost[p].begin(), cost[p].end()));
 //             int maxV = *max_element(cost[p].begin(), cost[p].end());
 //             if(minV > maxV) {
 //                 minV = maxV;
@@ -213,9 +216,8 @@ int main(int argc, char const *argv[])
 //             }
 //             // minV = min(minV, *max_element(cost[p].begin(), cost[p].end()));
 //         }
-//         // printf("minV : %d, minIdx : %d\n", minV, minIdx);
+//         printf("minV : %d, minIdx : %d\n", minV, minIdx);
 //         printf("%d\n", minIdx);
 //     }
 //     return 0;
 // }
-
